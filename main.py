5574cb8cd7fc56e495efd0e7e6d6520fc32c3fff
CelestialCrafter <celestialcraftermc@gmail.com>
2024-02-01 03:44:23 -0600

    add fire for cli

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np, pyautogui, cv2, time, sys
+import numpy as np, pyautogui, cv2, time, fire
 
 def mse(imageA, imageB):
 	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
@@ -6,30 +6,34 @@ def mse(imageA, imageB):
 
 	return err
 
-def execute(screenshot, name, threshold):
+def execute(screenshot, name, threshold, debug=''):
 	original = cv2.imread(f'data/{name}.png')
 	mask = cv2.imread(f'data/{name}_mask.png', 0)
 	error = mse(cv2.bitwise_and(original, original, mask=mask), cv2.bitwise_and(screenshot, screenshot, mask=mask))
-	if len(sys.argv) > 1 and sys.argv[1] == 'debug':
-		if len(sys.argv) > 2 and sys.argv[2] == name or sys.argv[2] == 'all':
-			print(f'{name} - {error}')
+	if debug == 'all' or debug == name:
+		print(f'{name} - {error}')
 	return error < threshold
 
-while True:
-	screenshot = pyautogui.screenshot()
-	screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+def main_loop(trailblaze_power_refill=True, debug=''):
+	while True:
+		screenshot = pyautogui.screenshot()
+		screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
 
-	if execute(screenshot, 'finish', 500):
-		pyautogui.click(x=1180, y=950)
+		if execute(screenshot, 'finish', 500, debug=debug):
+			pyautogui.click(x=1180, y=950)
 
-	elif execute(screenshot, 'resin', 450):
-		pyautogui.click(x=1180, y=735)
+		elif trailblaze_power_refill:
+			if execute(screenshot, 'resin', 450, debug=debug):
+				pyautogui.click(x=1180, y=735)
 
-	elif execute(screenshot, 'resin2', 100):
-		pyautogui.click(x=1400, y=690)
-		time.sleep(1)
-		pyautogui.click(x=1180, y=800)
-	elif execute(screenshot, 'resin3', 100):
-		pyautogui.click(x=950, y=950)
+			elif execute(screenshot, 'resin2', 100, debug=debug):
+				pyautogui.click(x=1400, y=690)
+				time.sleep(1)
+				pyautogui.click(x=1180, y=800)
+			elif execute(screenshot, 'resin3', 100, debug=debug):
+				pyautogui.click(x=950, y=950)
 
-	time.sleep(1 / 15)
+		time.sleep(1 / 15)
+
+if __name__ == '__main__':
+	fire.Fire(main_loop)
